ee80c9df1263e972117dc67e3e923f986d578a8e
Michelle Cinello <m.cinello@gmail.com>
2017-10-26 17:32:59 -0400

    can save new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,14 +20,29 @@ class ProjectsController < ApplicationController
     @project.title = params[:project][:title]
     @project.description = params[:project][:description]
     @project.goal = params[:project][:goal]
-    @project.start_date = params[:project][:start_date]
-    @project.end_date = params[:project][:end_date]
+
+
+    @project.start_date = DateTime.new(params[:project]["start_date(1i)"].to_i,
+    params[:project]["start_date(2i)"].to_i,
+    params[:project]["start_date(3i)"].to_i,
+    params[:project]["start_date(4i)"].to_i,
+    params[:project]["start_date(5i)"].to_i,
+    )
+
+    @project.end_date = DateTime.new(params[:project]["end_date(1i)"].to_i,
+    params[:project]["end_date(2i)"].to_i,
+    params[:project]["end_date(3i)"].to_i,
+    params[:project]["end_date(4i)"].to_i,
+    params[:project]["end_date(5i)"].to_i,
+    )
+
     @project.image = params[:project][:image]
-    @project.user_id = params[:project][:user_id]
+    @project.user_id = current_user.id
 
     if @project.save
-      redirect_to projects_url
+      redirect_to root_url
     else
+      flash.now[:alert] = @project.errors.full_messages
       render :new
     end
    end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  helper_method :current_user
 end
