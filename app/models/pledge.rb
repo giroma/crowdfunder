3467508f50385b2a0b3be27640eaa2de5e075e24
Michelle Cinello <m.cinello@gmail.com>
2017-10-26 16:09:03 -0400

    fixed errors

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,11 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner_cannot_back_own_project
+
+  def owner_cannot_back_own_project
+    if project.user == user
+      errors.add(:user_id, 'You cannot brack your own project.')
+    end
+  end
 end
