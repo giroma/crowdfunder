4890b320ac7f68151f121611ad4857ee428dfd68
Argir Argirov <dj.giro@gmail.com>
2017-10-26 16:07:50 -0400

    fixed 4 tests

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,6 +2,16 @@ class Pledge < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
+
+
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner_cant_pledge
+
+
+  def owner_cant_pledge
+    if project.user == user
+      errors.add(:user_id, "Owner cant pledge")
+    end
+  end
 end
