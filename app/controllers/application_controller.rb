54138c1fb9ea0c00bd2fa92fc7487a8ae76f35fb
horaciocoronel <arielc@gmail.com>
2017-10-30 15:39:52 -0400

    Add validation to show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,22 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+  helper_method :ensure_logged_in
+  helper_method :ensure_ownership
+
+  def ensure_logged_in
+    if !current_user
+      flash[:alert] = "please log in"
+      redirect_to root_url
+    end
+  end
+
+    def ensure_ownership
+      if current_user.id != @project.user_id
+        flash[:alert] = 'Not authorized owner'
+        redirect_to root_url
+      end
+  end
 
   private
   def not_authenticated
