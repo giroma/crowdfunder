1616409e4d994fdd3b1dcc0746f920b50899a27b
Michelle Cinello <m.cinello@gmail.com>
2017-11-01 12:28:45 -0400

    Added a form and edit view for Owner Update. Create controller methods and displayed the form on show page. A condition is set that only the owner of the project can post/edit/delete updates, and the updates can be viewed by a regular user.

--- app/controllers/owner_updates_controller.rb
+++ app/controllers/owner_updates_controller.rb
@@ -1,2 +1,46 @@
 class OwnerUpdatesController < ApplicationController
+  def create
+    @project_update = OwnerUpdate.create
+    @project = Project.find(params[:project_id])
+    @project_update.project_update = params[:owner_update][:project_update]
+    @project_update.project = @project
+    @project_update.user_id = @project.user_id
+
+    if @project_update.save
+      flash.notice = "Update has been successfully created."
+      redirect_to @project
+    else
+      render project_path(@project)
+    end
+  end
+
+  def edit
+    @project_update = OwnerUpdate.find(params[:id])
+    @project = Project.find(params[:project_id])
+  end
+
+  def update
+    @project_update = OwnerUpdate.find(params[:id])
+    @project = Project.find(params[:project_id])
+    @project_update.project_update = params[:owner_update][:project_update]
+    @project_update.project = @project
+
+    if @project_update.save
+      flash.notice = "Update has been successfully updated."
+      redirect_to @project
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project_update = OwnerUpdate.find(params[:id])
+
+    if @project_update.destroy
+      flash[:notice] = "Update has been successfully deleted."
+      redirect_to project_path(params[:project_id])
+    else
+      render :show
+    end
+  end
 end
