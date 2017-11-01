e2a891f4dddf908284e445839f130d62511f7f8d
horaciocoronel <arielc@gmail.com>
2017-11-01 15:28:41 -0400

    Add search for categories and projects, also add select category field when creating a new project

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @projects = Project.where("(title LIKE ?)", "%#{params[:search][:input].capitalize}%")
+    projects_search = Project.where("title ILIKE ?", "%#{params[:search][:input]}%").to_a
+    categories_search = Category.where("name ILIKE ?", "%#{params[:search][:input]}%").map {|c| c.projects}
+
+    # binding.pry
+    @projects = (projects_search + categories_search).flatten
+
+
   end
 end
