a2b2a018e8ad0e00b50b4cfef4ea986241b1d206
Michelle Cinello <m.cinello@gmail.com>
2017-11-01 15:05:39 -0400

    generated a Claims model and controller, added associations to models, added logic in create method in Claims controller

---
+++ app/controllers/claims_controller.rb
@@ -0,0 +1,14 @@
+class ClaimsController < ApplicationController
+
+  def create
+    @project = Project.find(params[:id])
+    @reward = Reward.find(params[:id])
+    @user = Pledge.where(user_id: current_user).where(project_id: @project) # user must have pledged to claim reward
+
+    @claim = Claim.new
+    @claim.project_id = @project.id
+    @claim.reward_id = @reward.id
+    @claim.user_id =@user.id
+  end
+
+end
