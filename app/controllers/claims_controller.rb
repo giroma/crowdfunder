95ab20152b64d49a5dbe10d37d9407cf99deb757
Michelle Cinello <m.cinello@gmail.com>
2017-11-02 09:29:58 -0400

    created claims methods in controller. claims are saving in database but having trouble displaying the information properly

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -4,11 +4,10 @@ class ClaimsController < ApplicationController
     @project = Project.find(params[:id])
     @reward = Reward.find(params[:id])
     @user = Pledge.where(user_id: current_user).where(project_id: @project) # user must have pledged to claim reward
-
-    @claim = Claim.new
+    @claim = Claim.create
     @claim.project_id = @project.id
     @claim.reward_id = @reward.id
-    @claim.user_id =@user.id
+    @claim.user_id = @user.id
   end
 
 end
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -10,7 +10,21 @@ class PledgesController < ApplicationController
     @user = @project.user_id
 
     if @pledge.save
-      redirect_to project_url(@project), notice: "You have successfully backed #{@project.title}!"
+      @claim = Claim.new
+      @rewards = @project.rewards
+      @reward = @rewards.where("dollar_amount < ?", @pledge.dollar_amount)
+      @reward.max_by do |reward|
+        reward.dollar_amount
+      end
+      @reward = @reward.to_a
+      @reward = @reward[0]
+
+      @claim = Claim.create(
+      project_id: @project.id,
+      user_id: @pledge.user.id,
+      reward_id: @reward[0]
+      )
+      redirect_to project_url(@project), notice: "You have successfully backed #{@project.title}, You've earned the reward #{@reward.description}!"
     else
       flash.now[:alert] = @pledge.errors.full_messages.first
       render 'projects/show'
